05d52a5d4e62c7ed6b03a212815c9c5bad7ba59d
samuel <samuel.martinez.morales@gmail.com>
2021-10-21 10:18:34 -0500

    Redirecting

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,17 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
 const NotFound = () => {
+
+    useEffect(() => {
+        const router = useRouter();
+        setTimeout(() =>{
+            //router.go(-1)
+            router.push('/')
+        }, 3000)
+
+    }, [])
+
     return ( 
         <div className="not-found">
             <h1>Oooops...</h1>
