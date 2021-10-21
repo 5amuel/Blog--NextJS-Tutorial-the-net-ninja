13e73523ec2e79b71ceb4c241ec0b612ed088f8d
samuel <samuel.martinez.morales@gmail.com>
2021-10-21 15:18:29 -0500

    Build

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 const About = () => {
     return ( 
         <>
@@ -7,8 +9,8 @@ const About = () => {
             </Head>
             <div>
                 <h1>About</h1>
-                <p className={styles.text} >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra tristique viverra. Nullam id sagittis nunc, sit amet ornare urna. Pellentesque scelerisque quis eros eget egestas. Maecenas non enim at nibh pulvinar finibus non a lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec.</p>
-                <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra tristique viverra. Nullam id sagittis nunc, sit amet ornare urna. Pellentesque scelerisque quis eros eget egestas. Maecenas non enim at nibh pulvinar finibus non a lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec.</p>
+                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra tristique viverra. Nullam id sagittis nunc, sit amet ornare urna. Pellentesque scelerisque quis eros eget egestas. Maecenas non enim at nibh pulvinar finibus non a lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec.</p>
+                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra tristique viverra. Nullam id sagittis nunc, sit amet ornare urna. Pellentesque scelerisque quis eros eget egestas. Maecenas non enim at nibh pulvinar finibus non a lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec.</p>
             </div>
         </>
      );
