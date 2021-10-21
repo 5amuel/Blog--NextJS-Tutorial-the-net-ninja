bb3cb44ca1b8c9353bf9e34629e9731d81eef394
samuel <samuel.martinez.morales@gmail.com>
2021-10-21 08:33:21 -0500

    Creating a Layout Component

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>lorem</p>
+            <p>lorem</p>
+            <p>lorem</p>
+            
+        </div>
+     );
+}
+ 
+export default About;
